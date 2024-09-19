85ce34e69410520fd502e62cf76be38614c55c4d
Samvel <tradeup.education@gmail.com>
2024-09-19 11:17:30 +0300

    Update module_3_5.ру.py

--- module_3_5.ру.py
+++ module_3_5.ру.py
@@ -1,13 +1,19 @@
 def get_multiplied_digits(number):
     str_number = str(number)
 
-    if len(str_number) == 1:
-        return int(str_number)
-
     first = int(str_number[0])
-    return first * get_multiplied_digits(
-        int(str_number[1:]))
+
+    if len(str_number) > 1:
+
+        if first == 0:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+
+            return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+
+        return first if first != 0 else 1
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(240)
 print(result)
